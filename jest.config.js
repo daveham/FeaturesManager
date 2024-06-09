a25b3e9763ecdd3b47710153ee3a4466da062bb4
Dave Hamilton <djhamilton2@protonmail.com>
2024-06-09 00:20:19 -0500

    Attempting TS support in jest.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.spec.json',
+      },
+    ],
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|js|jsx)$',
+  // TODO: get jest working with tsx files.
+  // testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js|jsx)$',
+  testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/'],
+  cacheDirectory: '.jest/cache',
 };
